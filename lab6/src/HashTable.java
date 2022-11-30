4f52e186444becf9aa40a927409cb5a9f6a2e414
Mercuuury <kovshiksa@gmail.com>
2022-11-30 21:38:25 +0300

    lab 6 done

--- lab6/src/HashTable.java
+++ lab6/src/HashTable.java
@@ -1,31 +1,46 @@
 public class HashTable {
     private Node[] arr;
-    private int arrSize;
     private Node deletedNode;
+    private static final int hashingConst = 5;
+    private boolean isLinear;
+    private int arrSize;
 
-    public HashTable(int size) {
+    public HashTable(int size, boolean isLinear) {
         arrSize = size;
         arr = new Node[arrSize];
         deletedNode = new Node(-1);
+        this.isLinear = isLinear ? true : false;
     }
 
     public int hashFunction(int key) {
         return key % arrSize;
     }
 
-    public void insert(Node item) {
-        int hash = hashFunction(item.getKey()); // Хэшируем ключ
+    public int hashFunction2(int key) {
+        return key * (key + 3) % arrSize;
+    }
+
+    public int hashFunctionDouble(int key) {
+        return hashingConst - key % hashingConst;
+    }
+
+    public void insert(Node item, int hashFunction) {
+        // Хешируем ключ
+        int hash = hashFunction == 1 ? hashFunction(item.getKey()) : hashFunction2(item.getKey());
+        int stepSize = hashFunctionDouble(item.getKey()); // Определяем размер шага для двойного хеширования
 
         while (arr[hash] != null && arr[hash].getKey() != -1) // Пока не найдем пустой/удаленный элемент
         {
-            ++hash;
+            hash += isLinear ? 1 : stepSize;
             hash %= arrSize;
         }
         arr[hash] = item;
     }
 
     public Node delete(int key) {
-        int hash = hashFunction(key); // Хэшируем ключ
+        int hash = hashFunction(key); // Хешируем ключ
+        int stepSize = hashFunctionDouble(key); // Определяем размер шага для двойного хеширования
+
         while (arr[hash] != null) // Пока не найдем пустой элемент
         {
             if (arr[hash].getKey() == key) { // Если нашли ключ
@@ -33,19 +48,22 @@ public class HashTable {
                 arr[hash] = deletedNode; // Удаляем элемент
                 return temp; // Возвращаем удаленный элемент
             }
-            ++hash;
+            hash += isLinear ? 1 : stepSize;
             hash %= arrSize;
         }
         return null;
     }
 
     public Node find(int key) {
-        int hash = hashFunction(key); // Хэшируем ключ
+        int hash = hashFunction(key); // Хешируем ключ
+        int stepSize = hashFunctionDouble(key); // Определяем размер шага для двойного хеширования
+
         while (arr[hash] != null) // Пока не найдем пустой элемент
         {
             if (arr[hash].getKey() == key) // Если нашли ключ
                 return arr[hash]; // Возвращаем элемент
-            ++hash;
+
+            hash += isLinear ? 1 : stepSize;
             hash %= arrSize;
         }
         return null;
@@ -54,17 +72,15 @@ public class HashTable {
     public void display() {
         System.out.print("HashTable: ");
         for (Node node : arr) {
-            if (node != null)
-                System.out.print(node.getKey() + " ");
-            else
+            if (node != null) {
+                int key = node.getKey();
+                if (key == -1)
+                    System.out.print("[X] ");
+                else
+                    System.out.print(node.getKey() + " ");
+            } else
                 System.out.print("[] ");
         }
-        // for (int j = 0; j < arrSize; j++) {
-        //     if (arr[j] != null)
-        //         System.out.print(arr[j].getKey() + " ");
-        //     else
-        //         System.out.print("[]");
-        // }
         System.out.println();
     }
 }
--- lab6/src/Main.java
+++ lab6/src/Main.java
@@ -2,26 +2,111 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Node aNode;
-        int aKey, size, n, keysPerCell;
+        Node node;
+        Long start, end;
+        int type, key, size, n;
+        boolean condition = true;
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter size of hash table: ");
+        System.out.print("Выберите метод пробирования: 1 - линейное пробирование, 2 - двойное хеширование.\n >> ");
+        type = in.nextInt();
+        System.out.print("Введите размер хэш-таблицы: ");
         size = in.nextInt();
-        System.out.print("Enter initial number of items: ");
+        if (type == 2 && !isPrime(size)) {
+            size = getPrime(size);
+            System.out.println("При использовании двойного хеширования размер таблицы должен быть простым числом. " +
+                    "Размер был увеличен до " + size);
+        }
+        System.out.print("Введите первоначальное количество элементов: ");
         n = in.nextInt();
-        keysPerCell = 10;
 
-        HashTable theHashTable = new HashTable(size);
-
+        HashTable theHashTable = new HashTable(size, type == 1 ? true : false);
         for (int j = 0; j < n; j++) {
-            aKey = (int) (Math.random() * keysPerCell * size);
-            aNode = new Node(aKey);
-            theHashTable.insert(aNode);
+            key = (int) (Math.random() * 10 * size);
+            node = new Node(key);
+            theHashTable.insert(node, 1);
         }
 
-        theHashTable.display();
+        System.out.print("Список действий:\n" +
+                "0 - Распечатать хеш-таблицу\n" +
+                "1 - Вставка\n" +
+                "2 - Удаление\n" +
+                "3 - Поиск\n" +
+                "4 - Сравнение хеш-функций\n" +
+                "5 - Выход");
+        while (condition) {
+            System.out.print("\nВыберите действие: ");
+            int action = in.nextInt();
+            System.out.println();
+            in.nextLine();
+            switch (action) {
+                case 0: // Печать
+                    theHashTable.display();
+                    System.out.println();
+                    break;
+                case 1: // Вставка
+                    System.out.print("Введите значение для вставки: ");
+                    key = in.nextInt();
+                    start = System.nanoTime();
+                    theHashTable.insert(new Node(key), 1);
+                    end = System.nanoTime();
+                    System.out.println("Время: " + (end - start) + " нс");
+                    break;
+                case 2: // Удаление
+                    System.out.print("Введите значение для удаления: ");
+                    key = in.nextInt();
+                    start = System.nanoTime();
+                    node = theHashTable.delete(key);
+                    end = System.nanoTime();
+                    System.out.println("Результат: " + node + ". Время: " + (end - start) + " нс");
+                    break;
+                case 3: // Поиск
+                    System.out.print("Введите значение для поиска: ");
+                    key = in.nextInt();
+                    start = System.nanoTime();
+                    node = theHashTable.find(key);
+                    end = System.nanoTime();
+                    System.out.println("Результат: " + node + ". Время: " + (end - start) + " нс");
+                    break;
+                case 4: // Сравнение хеш-функций
+                    HashTable ht1 = new HashTable(size, type == 1 ? true : false);
+                    HashTable ht2 = new HashTable(size, type == 1 ? true : false);
+                    Node[] nodes = new Node[n];
+                    for (int j = 0; j < n; j++) {
+                        key = (int) (Math.random() * 10 * size);
+                        nodes[j] = new Node(key);
+                    }
 
-        in.close();
+                    start = System.nanoTime();
+                    for (Node no : nodes)
+                        ht1.insert(no, 1);
+                    end = System.nanoTime();
+                    System.out.println("key%size. Время: " + (end - start) + " нс");
+
+                    start = System.nanoTime();
+                    for (Node no : nodes)
+                        ht2.insert(no, 2);
+                    end = System.nanoTime();
+                    System.out.println("key*(key+3)%size. Время: " + (end - start) + " нс");
+                    break;
+                case 5:
+                    in.close();
+                    condition = false;
+                    break;
+            }
+        }
+    }
+
+    private static int getPrime(int min) {
+        for (int j = min + 1; true; j++)
+            if (isPrime(j))
+                return j;
+    }
+
+    private static boolean isPrime(int n) {
+        for (int j = 2; (j * j <= n); j++)
+            if (n % j == 0)
+                return false;
+        return true;
     }
 }
